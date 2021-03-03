36d87d92ddeaf457805dee444210458e91b0cf2f
rocketfish <rocket.fish@mail.com>
2021-03-02 22:12:50 -0500

    changed env format and remove hide logs

--- vue.config.js
+++ vue.config.js
@@ -37,7 +37,7 @@ module.exports = {
     webpackConfig.plugins.delete('preload');
     webpackConfig.plugins.delete('prefetch');
     webpackConfig.plugins.delete('progress');
-    webpackConfig.plugins.delete('friendly-errors');
+    // webpackConfig.plugins.delete('friendly-errors');
 
     // console.log(webpackConfig.toConfig())
   },
